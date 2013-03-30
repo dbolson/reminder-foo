22a779da3e9c33b20039468b0d048884b5d5dea0
Danny Olson <dbolson@gmail.com>
2013-03-30 15:35:57 -0700

    Test event_lists/show action.

--- spec/controllers/event_lists_controller_spec.rb
+++ spec/controllers/event_lists_controller_spec.rb
@@ -1,4 +1,34 @@
 require 'spec_helper'
 
 describe EventListsController do
+  render_views
+
+  context 'json' do
+    describe '#index' do
+    end
+
+    describe '#show' do
+      def get_action
+        get :show, id: record.id, format: :json
+      end
+
+      let(:record) { FactoryGirl.create(:event_list) }
+
+      it 'is successful' do
+        get_action
+        response.status.should == 200
+      end
+
+      it 'is a json response' do
+        get_action
+        response.header['Content-Type'].should include('application/json')
+      end
+
+      it 'displays the attributes of the record' do
+        get_action
+        JSON.parse(response.body)['id'].should == record.id
+        JSON.parse(response.body)['name'].should == record.name
+      end
+    end
+  end
 end
--- spec/factories/event_lists.rb
+++ spec/factories/event_lists.rb
@@ -1,4 +1,5 @@
 FactoryGirl.define do
   factory :event_list do
+    name { Faker::Lorem.characters(5) }
   end
 end
