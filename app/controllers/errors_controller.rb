fab1fe5764169177665e7fba1f1472970b02436f
Danny Olson <dbolson@gmail.com>
2013-03-30 15:46:41 -0700

    Tests.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,6 @@ class ErrorsController < ApplicationController
   respond_to :json, :xml
 
   def not_found
-    puts "ErrorsController#not_found"
     render json: { error: env['action_dispatch.exception'].message },
            status: :not_found
   end
--- spec/controllers/event_lists_controller_spec.rb
+++ spec/controllers/event_lists_controller_spec.rb
@@ -8,27 +8,37 @@ describe EventListsController do
     end
 
     describe '#show' do
-      def do_action
-        get :show, id: record.id, format: :json
+      def do_action(id)
+        get :show, id: id, format: :json
       end
 
-      let(:record) { FactoryGirl.create(:event_list) }
+      context 'with a valid id' do
+        let(:record) { FactoryGirl.create(:event_list) }
 
-      it 'is successful' do
-        do_action
-        response.status.should == 200
+        it 'is successful' do
+          do_action(record.id)
+          response.status.should == 200
+        end
+
+        it 'is a json response' do
+          do_action(record.id)
+          response.header['Content-Type'].should include('application/json')
+        end
+
+        it 'displays the attributes of the record' do
+          do_action(record.id)
+          body = JSON.parse(response.body)
+          body['id'].should == record.id
+          body['name'].should == record.name
+        end
       end
 
-      it 'is a json response' do
-        do_action
-        response.header['Content-Type'].should include('application/json')
-      end
-
-      it 'displays the attributes of the record' do
-        do_action
-        body = JSON.parse(response.body)
-        body['id'].should == record.id
-        body['name'].should == record.name
+      context 'with an invalid id' do
+        it 'is unsuccessful' do
+          expect {
+            do_action(-1)
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
       end
     end
   end
