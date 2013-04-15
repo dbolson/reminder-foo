e91856d31864831c5615fe196129ef1becd1eb22
Danny Olson <dbolson@gmail.com>
2013-04-14 18:13:02 -0700

    Do not show db output in error message.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,13 @@ class ErrorsController < ApplicationController
   respond_to :json, :xml
 
   def not_found
-    render json: { error: env['action_dispatch.exception'].message },
+    render json: { error: error_message },
            status: :not_found
+  end
+
+  private
+
+  def error_message
+    env['action_dispatch.exception'].message.gsub(/\[.+\]/, '').strip
   end
 end
