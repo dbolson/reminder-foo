2a440b0a3a4c66cc6551571b76b81a65b7fa881a
Danny Olson <dbolson@gmail.com>
2013-04-20 16:02:39 -0700

    Add to acceptance tests.

--- spec/acceptance/event_lists_controller_spec.rb
+++ spec/acceptance/event_lists_controller_spec.rb
@@ -5,8 +5,13 @@ resource 'Event List' do
   header 'Accept', 'application/json'
   header 'Content-Type', 'application/json'
 
+  let(:event_lists) { stub(:event_lists) }
+  let(:account) { stub(:account, event_lists: event_lists) }
+
   before do
     Timecop.freeze(2000, 1, 1)
+    ApiKey.stub(:find_by_access_token)
+      .and_return(stub(:api_token, account: account))
   end
 
   get '/api/v1/event_lists' do
@@ -21,19 +26,13 @@ resource 'Event List' do
                                 name: 'event list 2',
                                 updated_at: Time.zone.now)
     }
-
-    let(:event_lists) { stub(:event_lists) }
-    let(:account) { stub(:account, event_lists: event_lists) }
+    let(:body) { JSON.parse(response_body) }
 
     before do
-      ApiKey.stub(:find_by_access_token)
-        .and_return(stub(:api_token, account: account))
       event_lists.stub(:all).and_return([event_list1, event_list2])
     end
 
-    let(:body) { JSON.parse(response_body) }
-
-    example_request 'return all event lists' do
+    example_request 'find all event lists' do
       expect(status).to eq(200)
 
       expect(body).to eq({
@@ -54,5 +53,37 @@ resource 'Event List' do
         'status' => 200
       })
     end
+  end
+
+  get '/api/v1/event_lists/1' do
+    let!(:event_list) {
+      FactoryGirl.build_stubbed(:event_list, id: '1',
+                                name: 'event list',
+                                updated_at: Time.zone.now)
+    }
+    let(:event_lists) { stub(:event_lists) }
+    let(:account) { stub(:account, event_lists: event_lists) }
+    let(:body) { JSON.parse(response_body) }
+
+    before do
+      event_lists.stub(:find).with('1').and_return(event_list)
+    end
+
+    example_request 'find an event list' do
+      expect(status).to eq(200)
+
+      expect(body).to eq({
+        'event_list' => {
+            'id' => 1,
+            'name' => 'event list',
+            'created_at' => '2000-01-01T00:00:00Z',
+            'updated_at' => '2000-01-01T00:00:00Z'
+          },
+        'status' => 200
+      })
+    end
+  end
+
+  post '/api/v1/event_lists/' do
   end
 end
