29b98bca9a4cf89d1027b775a77049c78a8cef5a
Danny Olson <dbolson@gmail.com>
2013-06-08 12:43:48 -0700

    Log response after errors.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,18 +2,25 @@ class ErrorsController < ApplicationController
   respond_to :json, :xml
 
   def not_found
-    render json: { errors: error_message },
-           status: :not_found
+    render json: { errors: exception_message }, status: :not_found
   end
 
   def internal_server_error
-    render json: { errors: 'Internal server error. We have been notified of the problem.' },
-           status: :internal_server_error
+    log_response
+    render json: response_body, status: :internal_server_error
   end
 
   private
 
-  def error_message
+  def exception_message
     env['action_dispatch.exception'].message.gsub(/\[.+\]/, '').strip
+  end
+
+  def log_response
+    Response.log(status: 500, content_type: 'application/json', body: response_body)
+  end
+
+  def response_body
+    { errors: 'Internal server error. We have been notified of the problem.' }
   end
 end
